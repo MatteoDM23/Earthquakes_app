988ae783e6b5c95de43c48d2d29598436784a3a7
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-10-11 13:37:46 +0000

    Signup page added

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   get 'static_pages/home'
-
   get 'static_pages/help'
-  
   get 'static_pages/about'
-  
   get 'static_pages/contact'
-
+  
+  get '/signup', to: 'users#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 
