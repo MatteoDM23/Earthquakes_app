6691a1f9ab7d74568e90152d5ea18dabc9549fb8
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-10-11 11:52:12 +0000

    generate parsing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006224638) do
+ActiveRecord::Schema.define(version: 20171011104925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20171006224638) do
     t.string "country"
     t.string "supra_region"
     t.string "region"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "earthquakes", force: :cascade do |t|
+    t.date "time"
+    t.integer "latitude"
+    t.integer "longitude"
+    t.integer "depth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
