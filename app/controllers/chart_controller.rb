fccd714c27cf4e3bb2793c7eee3bf24cb8afcb66
m.demichele.16@aberdeen.ac.uk <demiurgo1984@gmail.com>
2017-10-16 14:36:21 +0000

    login restrictions

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,4 +1,10 @@
 class ChartController < ApplicationController
+  before_action :is_logged_in
+  
+  def is_logged_in
+    redirect_to("/login") if current_user.blank?
+  end
+  
   def time
   end
 
